d99a736d02200f211719f1ef024f81ac75a89eb0
Andrew <aschnitkey@gmail.com>
2023-10-16 14:04:31 -0400

    lesson 11 - dashboard layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,4 @@
 	import '../app.css';
 </script>
 
-<nav class="flex flex-row justify-between w-full py-1 mb-5">
-	<h1 class="text-5xl font-bold font-handwriting">Everything Svelte</h1>
-	<div>
-		<a href="/" class="m-1">Home</a>
-		<a href="/signup" class="m-1">Signup</a>
-		<a href="/invoices/andrew-schnitkey">Andrew</a>
-	</div>
-</nav>
 <slot />
--- src/routes/invoices/[id]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let data;
-	console.log(data);
-</script>
-
-<h1>Invoice</h1>
-<p>Name: {data.id.replaceAll('-', ' ')}</p>
