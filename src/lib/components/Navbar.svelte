12b344d4aecac82da7c30bd859504e4744463a2e
Andrew <aschnitkey@gmail.com>
2023-10-18 11:14:24 -0400

    lesson 12 - current path

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class="col-span-3 text-center bg-daisyBush">
+	<div>
+		<a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
+	</div>
+
+	<nav>
+		<ul class="text-2xl font-bold list-none">
+			<li><a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a></li>
+			<li><a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a></li>
+			<li><a href="/settings" class:active={$page.url.pathname === '/settings'}>Settings</a></li>
+			<li><a href="/logout" class:active={$page.url.pathname === '/logout'}>Logout</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="postcss">
+	nav ul li {
+		@apply mb-6;
+	}
+	nav ul li a {
+		@apply font-bold text-white hover:text-goldenFizz;
+	}
+	nav ul li a.active {
+		@apply text-robinEggBlue px-8 transition-[padding];
+		background: url('/images/active-nav--left.svg') left top no-repeat,
+			url('/images/active-nav--right.svg') right top no-repeat;
+	}
+	nav ul li a.active:hover {
+		@apply px-10;
+	}
+</style>
--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,29 +1,10 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
 <div class="grid min-h-screen grid-cols-12 gap-x-16 bg-whisper">
-	<header class="col-span-3 text-center bg-daisyBush">
-		<div>
-			<a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
-		</div>
-
-		<nav>
-			<ul class="text-2xl font-bold list-none">
-				<li><a href="/invoices">Invoices</a></li>
-				<li><a href="/clients">Clients</a></li>
-				<li><a href="/invoices">Settings</a></li>
-				<li><a href="/invoices">Logout</a></li>
-			</ul>
-		</nav>
-	</header>
-
+	<Navbar />
 	<main class="col-span-8 pt-10">
 		<slot />
 	</main>
 </div>
-
-<style lang="postcss">
-	nav ul li {
-		@apply mb-6;
-	}
-	nav ul li a {
-		@apply font-bold text-white hover:text-goldenFizz;
-	}
-</style>
