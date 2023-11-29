43b138b67e542801fb7ef0511aa9283d7c333cce
Andrew <aschnitkey@gmail.com>
2023-11-28 21:25:58 -0500

    Lesson 49 - Styling the Error Page

---
+++ src/routes/(dashboard)/invoices/404/+page@.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>404 Error | The Dollar Holler</title>
+</svelte:head>
+
+<div class="w-screen h-screen bg-center bg-no-repeat center bg-error">
+	<div>
+		<h1 class="text-[3.5rem] font-black leading-none text-daisyBush text-center">Sorry!</h1>
+		<p class="text-lg text-center text-daisyBush">We can’t find the page you’re looking for.</p>
+	</div>
+</div>
