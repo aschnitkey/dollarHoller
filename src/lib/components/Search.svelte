79f0f53e93cdd0547e278043ac740db5226f2186
Andrew <aschnitkey@gmail.com>
2024-01-11 15:41:45 -0500

    Lesson 62 - Animating the Search Input field

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -10,7 +10,26 @@
 		type="text"
 		name="search"
 		placeholder="Search by keyword"
-		class="search w-full text-base lg:text-xl text-black bg-transparent border-b-2 border-dashed outline-none md:w-[14rem] lg:w-72 border-b-pastelPurple font-sansSerif focus:border-b-lavenderIndigo focus:border-solid"
+		class="search pr-[80px] w-full text-base lg:text-xl text-black bg-transparent border-b-2 border-dashed outline-none md:w-[14rem] lg:w-72 border-b-pastelPurple font-sansSerif focus:border-b-lavenderIndigo focus:border-solid"
 	/>
-	<button class="text-base font-black lg:text-xl font-sansSerif text-pastelPurple">Search</button>
+	<button
+		class="absolute text-base font-black transition-transform transform pointer-events-none left-8 lg:text-xl font-sansSerif text-pastelPurple"
+		>Search</button
+	>
 </div>
+
+<style lang="postcss">
+	::placeholder {
+		@apply text-transparent;
+	}
+
+	input:not(:placeholder-shown) + button,
+	input:focus + button {
+		@apply translate-x-[calc(100%-65px)] md:translate-x-[16rem] lg:translate-x-80 left-auto right-0 md:left-0 md:right-auto text-right text-lavenderIndigo pointer-events-auto;
+	}
+
+	input + button:focus,
+	input:focus + button:hover {
+		@apply text-daisyBush;
+	}
+</style>
