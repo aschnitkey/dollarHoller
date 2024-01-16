79ed13707a32bd5a41429842ed3fb6f70a691270
Andrew <aschnitkey@gmail.com>
2024-01-16 10:50:13 -0500

    Lesson 69 - Additional Options for Mobile

--- src/lib/actions/Swipe.ts
+++ src/lib/actions/Swipe.ts
@@ -8,7 +8,7 @@ interface SwipeProps {
 export const swipe: Action<HTMLElement, SwipeProps> = (node: HTMLElement, params: SwipeProps) => {
 	let x: number;
 	let startingX: number;
-	const elementWidth = node.clientWidth;
+	let elementWidth = node.clientWidth;
 	let triggerReset = params?.triggerReset || false;
 
 	const coordinates = spring(
@@ -23,7 +23,26 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node: HTMLElement, params
 		node.style.transform = `translate3d(${$coords.x}px, 0, 0)`;
 	});
 
-	node.addEventListener('mousedown', handleMouseDown);
+	if (isMobileBreakpoint()) {
+		node.addEventListener('mousedown', handleMouseDown);
+	}
+
+	window.addEventListener('resize', () => {
+		if (isMobileBreakpoint()) {
+			node.addEventListener('mousedown', handleMouseDown);
+		} else {
+			node.removeEventListener('mousedown', handleMouseDown);
+		}
+
+		elementWidth = node.clientWidth;
+	});
+
+	function isMobileBreakpoint() {
+		const mediaQuery = window.matchMedia('(max-width: 1024px)');
+		if (mediaQuery.matches) {
+			return true;
+		}
+	}
 
 	function resetCard() {
 		coordinates.update(() => {
