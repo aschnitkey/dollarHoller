571fcc54e0950ff7eae637284aedc823765e23a5
Andrew <aschnitkey@gmail.com>
2024-01-17 14:10:33 -0500

    Lesson 73 - env Variables

--- src/lib/utils/supabase.ts
+++ src/lib/utils/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { env } from '$env/dynamic/public';
+
+const supabaseUrl = env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = env.PUBLIC_SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-	'https://wfdwkqqeycwdxerzxuyl.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZHdrcXFleWN3ZHhlcnp4dXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1MDcyMzQsImV4cCI6MjAyMTA4MzIzNH0.JVEG5UrQRu2oCn6y3y0eJ3BP_tBRGrkTwUdT7mjAe5Y'
-);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
