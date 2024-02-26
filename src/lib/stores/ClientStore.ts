1875b579213786bfe6dacbf08e7881f695f36daa
Andrew <aschnitkey@gmail.com>
2024-02-26 15:13:41 -0500

    Lesson 85 - Edit a Client

--- src/lib/stores/ClientStore.ts
+++ src/lib/stores/ClientStore.ts
@@ -45,7 +45,20 @@ export const addClient = async (clientToAdd: Client) => {
 	return clientToAdd;
 };
 
-export const updateClient = (clientToUpdate: Client) => {
+export const updateClient = async (clientToUpdate: Client) => {
+	const { invoice, ...newClient } = clientToUpdate;
+
+	const { data, error } = await supabase
+		.from('client')
+		.update(newClient)
+		.eq('id', newClient.id)
+		.select();
+
+	if (error) {
+		displayErrorMessage(error as Error);
+		return;
+	}
+
 	clients.update((prev: Client[]) =>
 		prev.map((cur: Client) => (cur.id === clientToUpdate.id ? clientToUpdate : cur))
 	);
