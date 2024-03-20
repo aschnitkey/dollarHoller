364a147b0159920f38f23a8bb5be1aa54c99d493
Andrew <aschnitkey@gmail.com>
2024-03-20 14:54:12 -0400

    Lesson 97 - Revisit the Login Page for Server Side Authentication

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,40 +1,15 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-
-	let email: string;
-	let password: string;
-	let message: string = '';
-	let isLoading: boolean = false;
-
 	import Alert from '$lib/components/Alert.svelte';
-	import Loader from '$lib/components/Loader.svelte';
-	import Activate from '$lib/components/icon/Activate.svelte';
-	import { displayErrorMessage } from '$lib/utils/handleError';
-	import supabase from '$lib/utils/supabase';
 
-	const handleSubmit = async () => {
-		isLoading = true;
-		message = '';
-		let { data, error } = await supabase.auth.signInWithPassword({
-			email,
-			password
-		});
-
-		if (error) {
-			message = error.message;
-			console.error(error);
-		} else {
-			goto('/invoices');
-		}
-		isLoading = false;
-	};
+	export let form;
+	export let email = form?.values.email || '';
 </script>
 
 <h1 class="auth-heading">Login</h1>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<Alert {message} />
-	<fieldset disabled={isLoading}>
+<form method="POST">
+	<Alert message={form?.error} />
+	<fieldset>
 		<div class="field">
 			<label for="email" class="text-goldenFizz">Email Address</label>
 			<input type="email" name="email" id="email" placeholder="Your Email" bind:value={email} />
@@ -46,21 +21,11 @@
 					>Forgot?</a
 				>
 			</div>
-			<input
-				type="password"
-				name="password"
-				id="password"
-				placeholder="Your Password"
-				bind:value={password}
-			/>
+			<input type="password" name="password" id="password" placeholder="Your Password" />
 		</div>
 		<div class="field">
 			<button type="submit" class="flex items-center justify-center auth-submit gap-x-2">
-				{#if isLoading}
-					<Loader /> Loading...
-				{:else}
-					Let's do this!
-				{/if}
+				Let's do this!
 			</button>
 			<p class="mt-4 text-sm text-center text-white">
 				<a href="/signup" class="underline hover:no-underline">Dont' have an account yet?</a>
