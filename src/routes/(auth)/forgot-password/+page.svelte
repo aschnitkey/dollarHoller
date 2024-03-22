eaca0e57b69c08d25d9046a500985c254fab8709
Andrew <aschnitkey@gmail.com>
2024-03-22 10:29:40 -0400

    Lesson 99 - Handling Forgot Password

--- src/routes/(auth)/forgot-password/+page.svelte
+++ src/routes/(auth)/forgot-password/+page.svelte
@@ -1,40 +1,31 @@
 <script lang="ts">
-	import { displayErrorMessage } from '$lib/utils/handleError';
-	import supabase from '$lib/utils/supabase';
 	import Alert from '$lib/components/Alert.svelte';
 
-	let email: string;
-	let message = '';
-
-	const handleSubmit = async () => {
-		let { data, error } = await supabase.auth.resetPasswordForEmail(email);
-
-		if (error) {
-			displayErrorMessage(error);
-			return;
-		}
-
-		message = 'Check email for instructions';
-	};
+	export let form;
+	export let email = form?.values?.email || '';
 </script>
 
 <h1 class="auth-heading">Forgot Password</h1>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<Alert {message} />
-	<div class="field">
-		<label for="email" class="text-goldenFizz">Email Address</label>
-		<input
-			type="email"
-			name="email"
-			id="email"
-			placeholder="enter your email address"
-			required
-			bind:value={email}
-		/>
-	</div>
-	<button type="submit" class="auth-submit">Reset Password</button>
-	<p class="mt-4 text-sm text-center text-white">
-		<a href="/login" class="underline hover:no-underline">Ready to login?</a>
-	</p>
+<form method="POST">
+	{#if form?.success}
+		<Alert message={'Check email for password link'} />
+	{:else}
+		<Alert message={form?.error} />
+		<div class="field">
+			<label for="email" class="text-goldenFizz">Email Address</label>
+			<input
+				type="email"
+				name="email"
+				id="email"
+				placeholder="enter your email address"
+				required
+				bind:value={email}
+			/>
+		</div>
+		<button type="submit" class="auth-submit">Reset Password</button>
+		<p class="mt-4 text-sm text-center text-white">
+			<a href="/login" class="underline hover:no-underline">Ready to login?</a>
+		</p>
+	{/if}
 </form>
--- src/routes/(auth)/reset-password/+page.svelte
+++ src/routes/(auth)/reset-password/+page.svelte
@@ -1,6 +1,41 @@
+<script lang="ts">
+	import Alert from '$lib/components/Alert.svelte';
+	import supabase from '$lib/utils/supabase';
+	import { redirect } from '@sveltejs/kit';
+
+	let message: string = '';
+	let newPassword: string, confirmPassword: string;
+	let isLoading = false;
+
+	const handleSubmit = async () => {
+		isLoading = true;
+		message = '';
+
+		if (newPassword !== confirmPassword) {
+			message = 'Passwords Do Not Match';
+			isLoading = false;
+			return;
+		}
+		const { data, error } = await supabase.auth.updateUser({
+			password: newPassword
+		});
+
+		if (error) {
+			message = error.message;
+			console.error(error);
+			isLoading = false;
+			return;
+		}
+
+		isLoading = false;
+		redirect(303, '/login');
+	};
+</script>
+
+<Alert {message} />
 <h1 class="auth-heading">Reset My Password</h1>
 
-<form>
+<form on:submit={handleSubmit}>
 	<div class="field">
 		<label for="newPassword" class="text-goldenFizz">New Password</label>
 		<input
@@ -9,6 +44,7 @@
 			id="newPassword"
 			placeholder="enter new password"
 			required
+			bind:value={newPassword}
 		/>
 	</div>
 	<div class="field">
@@ -19,9 +55,10 @@
 			id="confirmPassword"
 			placeholder="confirm your new password"
 			required
+			bind:value={confirmPassword}
 		/>
 	</div>
-	<button type="submit" class="auth-submit">Update My Password</button>
+	<button type="submit" class="auth-submit">{isLoading ? 'Loading' : 'Update My Password'}</button>
 	<p class="mt-4 text-sm text-center text-white">
 		<a href="/login" class="underline hover:no-underline">I'm ready to login</a>
 	</p>
